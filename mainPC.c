f2a2e7c79ea950a08b1f8db45679f12a978ceab0
larchuto <johan.lafon@phelma.grenoble-inp.fr>
2015-10-01 23:49:24 +0200

    calcul de la frame à afficher fait séparément + display du texte comme il faut

--- mainPC.c
+++ mainPC.c
@@ -2,9 +2,12 @@
 #include <stdint.h>
 #include <unistd.h>
 
-#define FRAME_LEN 48
+#define LEN_TEXT 3
 
-static uint16_t frame[]=
+static int debut_frame=15;
+static uint16_t frame[16];
+
+static uint16_t text_display[]=
 {
 	0x0000, //            
 	0x0060, //      ##    
@@ -77,6 +80,32 @@ void print_column(uint16_t column)
 	printf(" |\n");
 }
 
+uint16_t get_text_line(int indice)
+{
+	/*static uint16_t frame[]=
+	{0b0000000000000000,
+	 0b0000000110000000,
+	 0b0000001110000000,
+	 0b0000011110000000,
+	 0b0000110110000000,
+	 0b0001100110000000,
+	 0b0000000110000000,
+	 0b0000000110000000,
+	 0b0000000110000000,
+	 0b0000000110000000,
+	 0b0000000110000000,
+	 0b0000000110000000,
+	 0b0000000110000000,
+	 0b0000111111110000,
+	 0b0000111111110000,
+	 0b0000000000000000,
+	 0b0000000000000000,
+	 0b0000000000000000,
+	 0b0000000000000000};*/
+
+	return text_display[indice%(LEN_TEXT*16)];
+}
+
 uint16_t get_frame_line(int indice)
 {
 	/*static uint16_t frame[]=
@@ -100,7 +129,7 @@ uint16_t get_frame_line(int indice)
 	 0b0000000000000000,
 	 0b0000000000000000};*/
 
-	return frame[indice%FRAME_LEN];
+	return frame[(debut_frame+indice)%16];
 }
 
 
@@ -110,29 +139,46 @@ uint16_t compute_column(int column_num, int pos)
 	int i;
 	for (i = 0; i < 16; i++)
 	{
-		column_value|=(((get_frame_line(i+pos))>>column_num)&1)<<(15-i);
+		column_value|=(((get_text_line(i+pos))>>(15-column_num))&1)<<(15-i);
 	}
 	return column_value;
 }
 
-void print_frame(int pos)
+void print_frame()
 {
 	int i;
 	printf(" ----------------------------------\n");
 	for(i=0;i<16;i++)
 	{
-		print_column(compute_column(i,pos));
+		print_column(get_frame_line(i));
 	}
 	printf(" ----------------------------------\n");
 }
 
+void compute_next_frame()
+{
+	static int char_actuel=0;
+	static int column_num=0;
+	frame[debut_frame]=compute_column(column_num,char_actuel*16);
+	debut_frame-=1;
+	if(debut_frame==-1)
+	{
+		debut_frame=15;
+	}
+	column_num+=1;
+	if(column_num==16)
+	{
+		char_actuel=(char_actuel+1)%LEN_TEXT;
+	}
+	column_num%=16;
+}
+
 void animation()
 {
-	static int pos=0;
-	print_frame(pos);
+	compute_next_frame();
+	print_frame();
 	printf("\033[2J\033[1;1H");
 	usleep(50000);
-	pos=(pos+1)%FRAME_LEN;
 } 
 
 int main()

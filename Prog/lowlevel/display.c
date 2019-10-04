90ea13bf0a47220b16babf9a89d35a1c057b12f1
pc-musique <Vous@exemple.com>
2019-10-04 17:37:31 +0200

    add function

--- Prog/lowlevel/display.c
+++ Prog/lowlevel/display.c
@@ -8,7 +8,7 @@
     HAL_GPIO_WritePin(DATA_GPIO_Port, DATA_Pin, gpio_state);		\
     HAL_GPIO_WritePin(BLANK_GPIO_Port, BLANK_Pin, gpio_state);		\
     HAL_GPIO_WritePin(PURGE_GPIO_Port, PURGE_Pin, gpio_state);		\
-  }									
+  }
 
 #define PURGE_CAPAS CAPAS_MGMT(GPIO_PIN_SET)
 #define REACTIVATE CAPAS_MGMT(GPIO_PIN_RESET)
@@ -52,46 +52,86 @@ void display_score(uint16_t score){
 
   //Indexs du score
   uint8_t index[3] = {score%10, (score/=10)%10, (score/=10)%10};
-  
+
   int frame, row;
   for(frame = 0; frame < BRIGHTNESS_LEVELS-1; frame++){
     for(row = 0; row < ROWS_PER_FRAME; row++){
       uint8_t *row_2 = sprites[index[2]][frame][row];
       uint8_t *row_1 = sprites[index[1]][frame][row];
       uint8_t *row_0 = sprites[index[0]][frame][row];
-      
+
       uint32_t data =
-	((uint32_t) ((((uint16_t) row_0[0]) << 8) | row_0[1]) << 16) |
+        ((uint32_t) ((((uint16_t) row_0[0]) << 8) | row_0[1]) << 16) |
         ((uint32_t) ((((uint16_t) row_1[0]) << 8) | row_1[1]) << 5) |
         ((uint32_t) ((((uint16_t) row_2[0]) << 8) | row_2[1]) >> 6);
-      
+
       //Attente du timer
       wait_refresh();
 
       //Purge capas
       PURGE_CAPAS;
-      
+
       //Sélection de la colonne
       SELECT_COL(9 + row);
 
       //Sélection des leds
-      HAL_GPIO_WritePin(LATCH_COL_GPIO_Port, LATCH_COL_Pin, GPIO_PIN_RESET);      
+      HAL_GPIO_WritePin(LATCH_COL_GPIO_Port, LATCH_COL_Pin, GPIO_PIN_RESET);
 
       int i;
       const uint32_t mask = 1 << 31;
       for(i = 32; i > 0; i--){
-	WRITE_BIT(((data&mask) >> 31));
-	data <<= 1;
+         WRITE_BIT(((data&mask) >> 31));
+         data <<= 1;
       }
-      
+
       HAL_GPIO_WritePin(LATCH_COL_GPIO_Port, LATCH_COL_Pin, GPIO_PIN_SET);
 
       //Réactivation
       REACTIVATE;
     }
   }
-  
+
 }
 
 
 
+
+void display_matrix(uint8_t matrix[32][32]){
+  for (int frame = 0; frame < BRIGHTNESS_LEVELS-1; frame++) {
+    for (int row = 0; row < ROWS_PER_FRAME; row++) {
+      uint8_t *row_2 = sprites[index[2]][frame][row];
+      uint8_t *row_1 = sprites[index[1]][frame][row];
+      uint8_t *row_0 = sprites[index[0]][frame][row];
+
+      uint32_t data =
+	((uint32_t) ((((uint16_t) row_0[0]) << 8) | row_0[1]) << 16) |
+        ((uint32_t) ((((uint16_t) row_1[0]) << 8) | row_1[1]) << 5) |
+        ((uint32_t) ((((uint16_t) row_2[0]) << 8) | row_2[1]) >> 6);
+
+      //Attente du timer
+      wait_refresh();
+
+      //Purge capas
+      PURGE_CAPAS;
+
+      //Sélection de la colonne
+      SELECT_COL(9 + row);
+
+      //Sélection des leds
+      HAL_GPIO_WritePin(LATCH_COL_GPIO_Port, LATCH_COL_Pin, GPIO_PIN_RESET);
+
+      int i;
+      const uint32_t mask = 1 << 31;
+      for(i = 32; i > 0; i--){
+	       WRITE_BIT(((data&mask) >> 31));
+         data <<= 1;
+      }
+
+      HAL_GPIO_WritePin(LATCH_COL_GPIO_Port, LATCH_COL_Pin, GPIO_PIN_SET);
+
+      //Réactivation
+      REACTIVATE;
+    }
+  }
+
+}
